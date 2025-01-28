9f37a09e2cd1f6b14ae2930b19ea6fffc5b47551
TsukasaMIURA32 <tsukasamiura@TsukasanoMacBook-Pro.local>
2025-01-28 21:02:08 +0800

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('admin', function($user){
             return $user->role_id == User::ADMIN_ROLE_ID;
         });
+
+        if($this->app->environment('production')){
+            URL::forceScheme('https');
+        }
     }
 }
